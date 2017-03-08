a9d66928f4b2edcb1f666087d849c28740e511f7
L3murCatta <l3murcatta@gmail.com>
2017-03-08 23:39:10 +0300

    Add files via upload

--- yadb.py
+++ yadb.py
@@ -1,28 +1,31 @@
 from random import randint
 from telegram.ext import Updater, CommandHandler
+digits = {0,1,2,3,4,5,6,7,8,9}
 
-def start(bot, update):
-    update.message.reply_text('Hello World!')
-
-def hello(bot, update):
-    update.message.reply_text('Hello {}'.format(\
-        update.message.from_user.first_name))
-    
 def dice(bot, update):
     message = update.message.text[6:]
-    parsed = message.split('d')
-    if len(parsed[0]) > 0:
-        num = int(parsed[0])
-    else:
+    d = message.find('d')
+    plus = message.find('+')
+    minus = message.find('-')
+    #excl = message.find('!')
+    if d == 0:
         num = 1
-    val = int(parsed[1])
-    res = [randint(1, val) for i in range(num)]
-    update.message.reply_text('{} rolled: {}, sum: {}'.format(update.message.from_user.username, res, sum(res)))
+    else:
+        num = int(message[:d])
+    sign = max(plus, minus)
+    if sign == -1:
+        val = message[d+1:]
+        mod = 0
+    else:
+        val = message[d+1:sign]
+        mod = message[sign+1:]
+        if message[sign] == '-':
+            mod = '-' + mod
+    res = [randint(1, int(val)) for i in range(num)]
+    update.message.reply_text('{} rolled: {}, total: {}'.format(update.message.from_user.username, res, sum(res)+int(mod)))
 
 updater = Updater('379931845:AAH-3mrlthdNokUKRx21PZ6rmIiYZZGp5vY')
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('dice', dice))
 
 updater.start_polling()
